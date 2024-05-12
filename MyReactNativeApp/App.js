cc767dac8453f22cd2d32ccf6888d800890aff84
Alexander Hughes <156909430+AlexanderHughes-Github@users.noreply.github.com>
2024-05-12 15:26:07 +0100

    Initial Commit

---
+++ MyReactNativeApp/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function AuthStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={FeedScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AuthStack />
+      {/* Conditionally render AuthStack or MainTabs based on authentication status */}
+    </NavigationContainer>
+  );
+}
+
